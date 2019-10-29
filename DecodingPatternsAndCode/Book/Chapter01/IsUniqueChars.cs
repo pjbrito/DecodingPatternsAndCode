5981530f4e5325abaff304b5d701cf2fe045fafd
Paulo Brito <pjbrito@gmail.com>
2019-10-29 20:45:39 +0000

    Added the 1st BookChapter01Test
    TheIsUniqueCharsAlgorithmShould - BeAbleToDetectRepeatedChars

---
+++ DecodingPatternsAndCode/Book/Chapter01/IsUniqueChars.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DecodingPatternsAndCode.Tests.Book.Chapter01
+{
+    public class IsUniqueCharsBaseCase
+    {
+        public static bool IsUniqueChars(string str)
+        {
+            if (str.Length > 128) return false;
+            var char_set = new bool[128];
+            for (var i = 0; i < str.Length; i++)
+            {
+                int val = str[i];
+                if (char_set[val]) // Already found this char in string
+                {
+                    return false;
+                }
+                char_set[val] = true;
+            }
+            return true;
+        }
+    }
+}
---
+++ DecodingPatternsAndCode/BookChapter01Tests.cs
@@ -0,0 +1,25 @@
+﻿using Xunit;
+using static DecodingPatternsAndCode.Tests.Book.Chapter01.IsUniqueCharsBaseCase;
+
+namespace DecodingPatternsAndCode.Tests
+{
+    public class BookChapter01Tests
+    {
+        public class TheIsUniqueCharsAlgorithmShould
+        {
+            [Theory]
+            [InlineData("MyBlackFoxWithDrugPens", true)]
+            [InlineData("BlackFoxPen", true)]
+            [InlineData("a", true)]
+            [InlineData("", true)]
+            [InlineData("AmazingChars", false)]
+            [InlineData("bb", false)]
+            public void BeAbleToDetectRepeatedChars(string inputString, bool expectedResult)
+            {
+                var actualResult = IsUniqueChars(inputString);
+                Assert.Equal(expectedResult, actualResult);
+            }
+        }
+    }
+
+}
