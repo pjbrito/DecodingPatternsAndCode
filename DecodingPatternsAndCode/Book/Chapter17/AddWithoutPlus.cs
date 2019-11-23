00563bf756b44e05501328e39fc233d3fa8318fe
Paulo Brito <pjbrito@gmail.com>
2019-11-23 22:53:49 +0000

    Added AddWithoutPlus

---
+++ DecodingPatternsAndCode/Book/Chapter17/AddWithoutPlus.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace DecodingPatternsAndCode.Tests.Book.Chapter17
+{
+    public class AddWithoutPlus
+    {
+        public int IntAddImpl(int a, int b)
+        {
+            while (b != 0)
+            {
+                var v1 = Convert.ToString(a, toBase: 2);
+                var v2 = Convert.ToString(b, toBase: 2);
+
+                var sum = (a ^ b);
+                var carry = (a & b) << 1;
+
+                var tmp = a + b;
+
+                a = sum;
+                b = carry;
+            }
+
+            return a;
+        }                                                                                                                                                                                                                                                                                                                                                                                                                                                                          
+    }
+}
---
+++ DecodingPatternsAndCode/BookTests/Chapter17/AddWithoutPlusTests.cs
@@ -0,0 +1,36 @@
+﻿using DecodingPatternsAndCode.Tests.Book.Chapter17;
+using Xunit;
+
+namespace DecodingPatternsAndCode.Tests.BookTests.Chapter17
+{
+    public class AddWithoutPlusTests
+    {
+        [Fact]
+        public void ShouldAddZerosCorrectly()
+        {
+            var uut = new AddWithoutPlus();
+            var result = uut.IntAddImpl(0, 0);
+
+            Assert.Equal(0, result);
+        }
+
+        [Fact]
+        public void ShouldAddOnesCorrectly()
+        {
+            var uut = new AddWithoutPlus();
+            var result = uut.IntAddImpl(1, 1);
+
+            Assert.Equal(2, result);
+        }
+
+        [Fact]
+        public void ShouldAddThreeDigitsCorrectly()
+        {
+            var uut = new AddWithoutPlus();
+            var result = uut.IntAddImpl(741, 123);
+
+            Assert.Equal(864, result);
+        }
+
+    }
+}
